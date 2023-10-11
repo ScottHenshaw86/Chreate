ed8ae19b2ce3ecc4684ca3ff2760048f309f23d8
Teerath Bajaj <teerathbajaj@Teeraths-MacBook-Pro.local>
2023-10-11 16:16:26 +0900

    the frontend modal

--- userProfile.php
+++ userProfile.php
@@ -4,16 +4,16 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Profile_Page</title>
+    <title>User_Page</title>
     <!-- boxICONS -->
     <link
       href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
       rel="stylesheet"
     />
     <!-- CSS link -->
-    <link rel="stylesheet" href="public/css/profilepage.css" />
+    <link rel="stylesheet" href="public/css/userProfile.css" />
     <!-- JS link -->
-    <script defer src="public/js/profilePage.js"></script>
+    <script defer src="public/js/userProfile.js"></script>
   </head>
   <body>
     <div class="header-wrapper">
@@ -59,7 +59,6 @@
           <!-- PHOTOS -->
 
           <div class="photos">
-           
             <?php
                 foreach ($posts as $post) {
                     include "./view/components/postFeedDisplay.php";
