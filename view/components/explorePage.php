f2b6ca4c3560247df8451aa7f34b37ddd2955fb0
Teerath Bajaj <teerathbajaj@Teeraths-MacBook-Pro.local>
2023-10-16 15:47:37 +0900

    made explorer page and made posts desc

--- view/components/explorePage.php
+++ view/components/explorePage.php
@@ -5,10 +5,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Explore Page</title>
+    <!-- CSS link -->
+    <link rel="stylesheet" href="./public/css/explore.css">
+    <!-- boxICONS -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
 </head>
 
 <body>
-    <h1>EXPLORE PAGE</h1>
+    <!-- NAVBAR -->
+    <a href="./index.php?action=challenge">
+        <button class="btn challenge-btn">Challenge of the Day</button>
+    </a>
+
+    <div class="navbar">
+        <div class="logo">
+            <a href="./index.php?action=indexView.php"><img src="./public/images/logo2.png" alt="" /></a>
+        </div>
+        <a href="./index.php?action=search" class="icons searchButton tooltip">
+            <i class="bx bx-search"></i>
+            <span class="tooltiptext">Search</span>
+        </a>
+        <a href="./index.php?action=explore" class="icons tooltip">
+            <i class="bx bx-compass"></i>
+            <span class="tooltiptext">Explore</span>
+        </a>
+        <a href="./index.php?action=newPostForm" class="icons tooltip">
+            <i class="bx bxs-camera-plus"></i>
+            <span class="tooltiptext">Add Post</span>
+        </a>
+        <a href="./index.php?action=challenge" class="icons tooltip">
+            <i class='bx bx-party'></i>
+            <span class="tooltiptext">Challenge</span>
+        </a>
+        <a href="index.php?action=logOut" class="icons tooltip">
+            <i class="bx bx-log-out-circle"></i>
+            <span class="tooltiptext">Log Out</span>
+        </a>
+    </div>
 
     <div class="searchButton">
         <form action="index.php?action=searchingChallenges" method="POST">
@@ -17,16 +51,69 @@
         </form>
     </div>
 
-    <div class="feedContainer">
+    <!-- <div class="feedContainer">
         <?php
         foreach ($posts as $post) {
             include "./view/components/postFeedDisplay.php";
         }
         ?>
 
-    </div>
+    </div> -->
 
+    <!-- photo gallery -->
+    <div class="container">
+        <div class="photo-gallery">
+            <div class="column">
+                <div class="photo">
+                    <div class="box">
+                        <img src="./public/images/g.jpg" alt="">
+                    </div>
+                    <div class="box">
+                        <img src="./public/images/img_5950.jpg" alt="">
+                    </div>
+                    <div class="box">
+                        <img src="./public/images/img_5951.jpg" alt="">
+                    </div>
+                </div>
+            </div>
 
+            <div class="column">
+                <div class="photo">
+                    <div class="box">
+                        <img src="./public/images/picc.jpg" alt="">
+                    </div>
+                    <div class="box">
+                        <img src="./public/images/xs.jpg" alt="">
+                    </div>
+                    <div class="box">
+                        <img src="./public/images/x.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="photo">
+                    <div class="box">
+                        <img src="./public/images/pic.jpg" alt="">
+                    </div>
+                    <div class="box">
+                        <img src="./public/images/temp_tide.jpg" alt="">
+                    </div>
+                    <div class="box">
+                        <img src="./public/images/ramsssss.png" alt="">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <!-- User Profile -->
+        <div class="user-profile">
+            <a href="./index.php?action=profilePage">
+                <img src="<?= $_SESSION['profilePic'] ?? './public/images/user/user1.webp' ?>" alt="">
+                <h4><?= htmlspecialchars($_SESSION['username']) ?></h4>
+            </a>
+        </div>
 </body>
 
 </html>
